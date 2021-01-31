75de4450bcd03673f928ec9e8c9fa77ee2db4af0
Axelshito <axel.arroyo.gimenez@gmail.com>
2021-01-31 13:03:33 +0100

    .

---
+++ html/Comandos basicos 1 (JS)/main.js
@@ -0,0 +1,6 @@
+document.write("<h1>Hola Mundo</h1>");
+window.alert("te saludo")
+window.alert("te saludo otra vez por si no te has percatado")
+window.alert("Esta es la 3 vez que te saludo")
+window.alert("Fin")
+
