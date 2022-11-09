f75b0cac98c42288bd77b817dc6d53b4ee233ff8
Axel <axel.arroyo.gimenez@gmail.com>
2022-11-09 19:22:12 +0100

    si

--- DAW/2ndo/Módulo 6 - Desarrollo web entorno cliente/Calendario/src/app/app.component.ts
+++ DAW/2ndo/Módulo 6 - Desarrollo web entorno cliente/Calendario/src/app/app.component.ts
@@ -86,15 +86,29 @@ export class AppComponent {
       31,
     ];
   }
+  setFestive(day: number, month: string) {
+    for (let i = 0; i < festivos.length; i++) {
+      if (day == festivos[i]['day'] && month == festivos[i]['month']) {
+        return festivos[i]['festive'];
+      }
+    }
+    return '';
+  }
+
+  monthDay(){
+    for(let i = 0; i < this.octubre.length; i++){
+      
+    }
+  }
+
+
 }
 
+type diasFestivos = Array<{ day: number; month: string; days: number,festive: string }>;
 
-
-type diasFestivos = Array<{day:number, month:number, festive:string}>;
-
-const festivos : diasFestivos =[
-  {day: 11, month:9, festive:'fiestaReguional'},
-  {day: 29, month:9, festive:'fiestaLocal'},
-  {day: 12, month:10, festive:'fiestaNacional'},
-  {day: 31, month:10, festive:'fiestaNacional'},
+const festivos: diasFestivos = [
+  { day: 12, month: 'octubre', days: 31, festive: 'fiestaNacional' },
+  { day: 31, month: 'octubre', days: 31,festive: 'fiestaCentro' },
+  { day: 11, month: 'septiembre',days: 30, festive: 'fiestaRegional' },
+  { day: 29, month: 'septiembre', days: 30,festive: 'fiestaLocal' },
 ];
