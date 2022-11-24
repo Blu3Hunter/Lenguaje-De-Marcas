f8b14b47cf1e7f888605185263371d07ad2d1341
Axel <axel.arroyo.gimenez@gmail.com>
2022-11-24 18:54:17 +0100

    Añadida carpeta vendor

--- DAW/2ndo/Módulo 6 - Desarrollo web entorno cliente/kanban-board/src/app/tarea/tarea.component.ts
+++ DAW/2ndo/Módulo 6 - Desarrollo web entorno cliente/kanban-board/src/app/tarea/tarea.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Tarea } from '../models/tarea-model';
+const k_PENDIENTES_LISTA: string = 'Pendientes';
+const k_PROGRESO_LISTA: string = 'Progreso';
+const k_FINALIZADAS_LISTA: string = 'Finalizadas';
 @Component({
   selector: 'app-tarea',
   templateUrl: './tarea.component.html',
-  styleUrls: ['./tarea.component.css']
+  styleUrls: ['./tarea.component.css'],
 })
 export class TareaComponent implements OnInit {
+  listas: string[] = [];
+  tareas: Tarea[];
 
-  constructor() { }
+  constructor() {
+    const tareasJSON: string = `{
+      "tareas": [
+        { "lista": "${k_FINALIZADAS_LISTA}", "img": "https://picsum.photos/300/200", "titulo": "Tarea 1: Diseño UI", "usuarios": [{"img": "https://picsum.photos/300/300", "alt": "Usuario"}], "fechaFin": "2019-01-16" },
+        { "lista": "${k_PROGRESO_LISTA}", "img": "https://picsum.photos/300/200", "titulo": "Tarea 2: Diseño de todo el Backend", "usuarios": [], "fechaFin": "2022-11-09" },
+        { "lista": "${k_PENDIENTES_LISTA}", "img": null, "titulo": "Tarea 3: Diseño de la base de datos", "usuarios": [{"img": "https://picsum.photos/300/300", "alt": "Usuario"}, {"img": "https://picsum.photos/300/300", "alt": "Usuario"}], "fechaFin": "2022-11-16" },
+        { "lista": "${k_PENDIENTES_LISTA}", "img": null, "titulo": "Tarea 4: Implementar todo el Front-End", "usuarios": [], "fechaFin": null }
+      ]
+    }`;
 
-  ngOnInit(): void {
+    const tareasDict: any = JSON.parse(tareasJSON);
+    this.tareas = tareasDict['tareas'];
+
+    this.listas.push(k_PENDIENTES_LISTA);
+    this.listas.push(k_PROGRESO_LISTA);
+    this.listas.push(k_FINALIZADAS_LISTA);
   }
 
+  ngOnInit(): void {}
 }
