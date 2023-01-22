562fbac89729978de5919fbc40e7cbae59a1cd92
Axel <axel.arroyo.gimenez@gmail.com>
2023-01-22 21:14:49 +0100

    Practica finalizada.

--- DAW/2ndo/Módulo 7 - Desarrollo web en entorno servidor/Laravel-CRUD/app/Http/Controllers/HomeController.php
+++ DAW/2ndo/Módulo 7 - Desarrollo web en entorno servidor/Laravel-CRUD/app/Http/Controllers/HomeController.php
@@ -11,27 +11,31 @@ use App\Models\User;
 class HomeController extends Controller
 {
 
+    /**
+     * Devolvemos la vista de la página inicial.
+     */
     public function index()
-    
+
     {
-        return view ('home.userpage');
+        return view('home.userpage');
     }
+
+    /**
+     * Con esta función estamos haciendo que si la variable del usuario es igual
+     * a 1, entonces es administrador, por ende, podrá acceder al admin panel. 
+     * En caso contrario y que no sea administrador se le redireccionará a la página
+     * inicial.
+     */
 
 
     public function redirect()
     {
         $usertype = Auth::user()->usertype;
 
-        if ($usertype == '1')
-        {
-            return view('admin.home');    
+        if ($usertype == '1') {
+            return view('admin.home');
         } else {
             return view('home.userpage');
         }
     }
-
-
-
-
-
 }
